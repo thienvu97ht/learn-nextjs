17f3e6c394a13f3e9620ac56c68e0d34845d3fa0
Duong Vu <thien.vu97ht123@gmail.com>
2021-12-27 15:39:52 +0700

    NextJS: 02-03 Navigation between Pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,23 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  const router = useRouter()
+
+  const gotoDetailPage = () => {
+    router.push({
+      pathname: '/posts/[postId]',
+      query: {
+        postId: 123,
+        ref: 'social',
+      },
+    })
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -20,6 +34,8 @@ const Home: NextPage = () => {
         <p className={styles.description}>
           Get started by editing <code className={styles.code}>pages/index.tsx</code>
         </p>
+
+        <button onClick={gotoDetailPage}>Go to post detail page</button>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
@@ -45,6 +61,12 @@ const Home: NextPage = () => {
             <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
           </a>
         </div>
+
+        <div style={{ marginTop: '2000px' }}></div>
+
+        <Link href="/about">
+          <a>Go to about</a>
+        </Link>
       </main>
 
       <footer className={styles.footer}>
