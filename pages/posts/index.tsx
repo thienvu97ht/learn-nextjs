55c3256337da0c343e7d79f02742f6cd32df775e
Duong Vu <thien.vu97ht123@gmail.com>
2022-01-02 00:08:17 +0700

    NextJS: 02-06 SSG getStaticProps with typescript

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,7 +1,23 @@
+import { GetStaticProps, GetStaticPropsContext } from 'next'
 import * as React from 'react'
 
-export interface PostListPageProps {}
+export interface PostListPageProps {
+  post: any[]
+}
 
 export default function PostListPage(props: PostListPageProps) {
   return <div>Post List Page</div>
+}
+
+export const getStaticProps: GetStaticProps<PostListPageProps> = async (
+  context: GetStaticPropsContext
+) => {
+  // server-side
+  // run lúc build-time
+
+  return {
+    props: {
+      post: [],
+    },
+  }
 }
